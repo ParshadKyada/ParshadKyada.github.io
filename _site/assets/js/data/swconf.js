af88a58d64554f684adc4aed2bf173a5bbd580f9
KrishV9055 <krish.vce21@sot.pdpu.ac.in>
2024-04-23 23:55:19 +0530

    final commit

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1713893897',resources: [
+    cacheName: 'chirpy-1713893917',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
