ca44f8d55f83885c7bc1f6beb749b10fc1a7c79b
ParshadKyada <parshadkyada2003@gmail.com>
2024-04-24 00:02:13 +0530

    final commit 1

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1713897010',resources: [
+    cacheName: 'chirpy-1713897125',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
